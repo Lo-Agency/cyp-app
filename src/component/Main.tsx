95ee2a8c782604ae534f1bedba31deafeebfb5c8
Somayeh Rahmani <rahmani.it27@gmail.com>
2025-04-12 17:00:06 +0330

    add button and style

--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -1,8 +1,15 @@
 function Main() {
   return (
-    <div className="bg-blue-900 h-60 bg-auto font-Yekan w-screen">
-      <h1 dir="rtl">با CYP کنترل مالی و پولت به دستت بگیر</h1>
-      <button></button>
+    <div className="flex flex-col justify-center items-center text-white bg-blue-900 h-60 bg-auto font-Yekan w-screen">
+      <div dir="rtl" className="m-3 text-base block">
+        با CYP کنترل مالی و پولت به دستت بگیر
+      </div>
+      <div dir="rtl" className="m-6 text-base block">
+        اپلیکیشنی ساده و کاربردی برای مدیریت دخل و خرج
+      </div>
+      <button className="bg-blue-500 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 ... w-32 h-10 rounded-xl">
+        شروع رایگان
+      </button>
     </div>
   );
 }
