4f17ccb1d1320265305dd3085604d75f4fb70f52
Somayeh Rahmani <rahmani.it27@gmail.com>
2025-04-28 04:15:05 +0330

    feat: add Dashboard component and update routing to use consistent path casing

---
+++ src/component/dashbord/Dashbord.tsx
@@ -0,0 +1,11 @@
+export default function Dashbord() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <div className="bg-white rounded-2xl shadow flex overflow-hidden min-h-screen">
+        <div className="w-64 bg-white border-r border-gray-300">
+          <h1 className="text-2xl font-bold mb-4 p-6">Dashboard</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
