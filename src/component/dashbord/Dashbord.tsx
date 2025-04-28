50d3cf80c5df4ac467ba42f332dd6ca90cf4a1cc
Somayeh Rahmani <rahmani.it27@gmail.com>
2025-04-28 04:57:59 +0330

    feat: add buttons for Income, Expenses, and Budgeted sections in Dashboard component

--- src/component/dashbord/Dashbord.tsx
+++ src/component/dashbord/Dashbord.tsx
@@ -5,6 +5,17 @@ export default function Dashbord() {
         <div className="w-64 bg-white border-r border-gray-300">
           <h1 className="text-2xl font-bold mb-4 p-6">Dashboard</h1>
         </div>
+        <div className="flex gap-4 p-8">
+          <button className="flex flex-col items-start bg-gray-50 border border-gray-200 rounded-lg p-4 w-40 h-20">
+            <span>Incom</span>
+          </button>
+          <button className="flex flex-col items-start bg-gray-50 border border-gray-200 rounded-lg p-4 w-40 h-20">
+            <span>Expenses</span>
+          </button>
+          <button className="flex flex-col items-start bg-gray-50 border border-gray-200 rounded-lg p-4 w-40 h-20">
+            <span>Budgeted</span>
+          </button>
+        </div>
       </div>
     </div>
   );
