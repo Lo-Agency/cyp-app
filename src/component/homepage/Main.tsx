725011558ef1bf9ea9feded388a62313623c1f9a
Somayeh Rahmani <rahmani.it27@gmail.com>
2025-05-13 11:09:23 +0330

    fix some issues

--- src/component/homepage/Main.tsx
+++ src/component/homepage/Main.tsx
@@ -1,4 +1,4 @@
-function Main({ onLoginClick }: { onLoginClick: () => void }) {
+function Main() {
   return (
     <div className="flex flex-col justify-center items-center text-white bg-blue-900 h-60 bg-auto font-Yekan w-screen">
       <div dir="rtl" className="m-3 text-base block">
@@ -7,10 +7,7 @@ function Main({ onLoginClick }: { onLoginClick: () => void }) {
       <div dir="rtl" className="m-6 text-base block">
         اپلیکیشنی ساده و کاربردی برای مدیریت دخل و خرج
       </div>
-      <button
-        className="bg-blue-500 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 w-32 h-10 rounded-xl"
-        onClick={onLoginClick}
-      >
+      <button className="bg-blue-500 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 w-32 h-10 rounded-xl">
         شروع رایگان
       </button>
     </div>
