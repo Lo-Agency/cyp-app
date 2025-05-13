4623252e405ea3c289db9e2a3102d63519868023
Somayeh Rahmani <rahmani.it27@gmail.com>
2025-05-13 12:14:30 +0330

    add index file and fix some thing

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,18 @@
-import { useState } from "react";
 import Dashbord from "./component/dashbord/Dashbord";
-import Header from "./component/homepage/Header";
-import Main from "./component/homepage/Main";
-import LoginModal from "./component/homepage/LoginModal";
-import RegisterModal from "./component/homepage/RegisterModal";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NotFound from "./component/NotFound";
+import HomePage from "./component/homepage/Index";
 
 function App() {
-  const [modalType, setModalType] = useState<null | "login" | "register">(null);
-
-  const closeModal = () => setModalType(null);
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Header />
-                <Main />
-              </>
-            }
-          />
-          <Route path="/Dashbord" element={<Dashbord />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/somayeh" element={<Dashbord />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
-
-      {modalType === "login" && (
-        <LoginModal
-          onClose={closeModal}
-          onSwitchToRegister={() => setModalType("register")}
-        />
-      )}
-
-      {modalType === "register" && (
-        <RegisterModal
-          onClose={closeModal}
-          onSwitchToLogin={() => setModalType("login")}
-        />
-      )}
     </>
   );
 }
--- src/component/homepage/Main.tsx
+++ src/component/homepage/Main.tsx
@@ -1,4 +1,8 @@
-function Main() {
+interface MainProps {
+  setModalType: (ModalType: null | "login" | "register") => void;
+}
+
+const Main: React.FC<MainProps> = ({ setModalType }) => {
   return (
     <div className="flex flex-col justify-center items-center text-white bg-blue-900 h-60 bg-auto font-Yekan w-screen">
       <div dir="rtl" className="m-3 text-base block">
@@ -7,10 +11,13 @@ function Main() {
       <div dir="rtl" className="m-6 text-base block">
         اپلیکیشنی ساده و کاربردی برای مدیریت دخل و خرج
       </div>
-      <button className="bg-blue-500 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 w-32 h-10 rounded-xl">
+      <button
+        className="bg-blue-500 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 w-32 h-10 rounded-xl"
+        onClick={() => setModalType("login")}
+      >
         شروع رایگان
       </button>
     </div>
   );
-}
+};
 export default Main;
