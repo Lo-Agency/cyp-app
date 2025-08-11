58f47bdcd2d5ab681c98add0f738526787791702
Somayeh Rahmani <rahmani.it27@gmail.com>
2025-08-11 12:03:04 +0330

    add shadcn to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
